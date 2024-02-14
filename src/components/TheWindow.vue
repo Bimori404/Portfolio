9b77603a8215fe7d719681db3bf6bef85aedd021
/Bimori <106877704+Bimori404@users.noreply.github.com>
2024-02-13 22:03:31 -0600

    a ver si no la cague

--- src/components/TheWindow.vue
+++ src/components/TheWindow.vue
@@ -19,7 +19,7 @@ export default {
 
 <template>
   <div :class="`bg-[var(--color-${color})] rounded-xl border-4 border-black shadow-solid`">
-    <div class="p-2 flex items-center border-b-2 gap-2 border-black">
+    <div class="p-2 flex items-center border-b-2 gap-2 border-black" color="primary">
       <div v-for="_ in decorations" class="w-5 h-5 border-2 border-black bg-white rounded-xl"></div>
       <div class="grow text-center font-bold">{{ title }}</div>
     </div>
