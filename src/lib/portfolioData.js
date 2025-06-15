f72bbec4aab008513aadd075fc91061b0ae6b046
/Bimori <106877704+Bimori404@users.noreply.github.com>
2025-06-15 09:38:49 -0600

    wip

--- src/lib/portfolioData.js
+++ src/lib/portfolioData.js
@@ -20,7 +20,7 @@ export const portfolioData = {
             {
                 name: "GitHub",
                 icon: "fa-brands fa-github",
-                url: "https://github.com/Bimori404"
+                url: "https://github.com/fwcalledsam"
             },
             {
                 name: "CV",
